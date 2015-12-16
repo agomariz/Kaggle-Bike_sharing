edcc51474ee07aa1aa7fe1cdb2e513dbd662ed19
agomariz <ant.gomariz@gmail.com>
2015-12-16 23:17:32 +0000

    get dummies for all categorical columns

--- Bike sharing/Preprocessing.py
+++ Bike sharing/Preprocessing.py
@@ -1,12 +1,18 @@
 import pandas as pd
 
-def _create_dummies(df,column):
+def _create_dummy(df,column):
     '''
     Exchange a categorical column by dummy columns in a Pandas dataframe
     :param df:
     :param column:
     :return:
     '''
-    dummy_df = pd.get_dummies(df[column], prefix='Season')
+    dummy_df = pd.get_dummies(df[column], prefix=column)
     df = pd.concat([df,dummy_df],axis=1)
     return df.drop(column, 1)
+
+def _create_dummies(df,columns):
+    for c in columns:
+        df = _create_dummy(df,c)
+    return df
+
--- Bike sharing/main.py
+++ Bike sharing/main.py
@@ -3,4 +3,4 @@ import Preprocessing as prep
 
 train_data = pd.read_csv('data/train.csv')
 
-print prep.create_dummies(train_data,'season').describe()
+print prep._create_dummies(train_data,['season','holiday','workingday','weather']).describe()
