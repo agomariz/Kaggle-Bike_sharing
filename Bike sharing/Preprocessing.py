c78c490fcb8a6fbacd9efe9489772bca8b8ee9f3
agomariz <ant.gomariz@gmail.com>
2015-12-16 23:59:50 +0000

    added function to split the datetime into different fields (year,month,day,hour)

--- Bike sharing/Preprocessing.py
+++ Bike sharing/Preprocessing.py
@@ -11,8 +11,16 @@ def _create_dummy(df,column):
     df = pd.concat([df,dummy_df],axis=1)
     return df.drop(column, 1)
 
-def _create_dummies(df,columns):
+def create_dummies(df, columns):
     for c in columns:
         df = _create_dummy(df,c)
     return df
+
+def process_date_column(df,column):
+    df['year'] = pd.to_datetime(df[column]).dt.year
+    df['month'] = pd.to_datetime(df[column]).dt.month
+    df['day'] = pd.to_datetime(df[column]).dt.day
+    df['hour'] = pd.to_datetime(df[column]).dt.hour
+
+    return df.drop(column, 1)
 
--- Bike sharing/main.py
+++ Bike sharing/main.py
@@ -2,5 +2,8 @@ import pandas as pd
 import Preprocessing as prep
 
 train_data = pd.read_csv('data/train.csv')
+train_data = prep.process_date_column(train_data,'datetime')
 
-print prep._create_dummies(train_data,['season','holiday','workingday','weather']).describe()
+print train_data.describe()
+
+print prep.create_dummies(train_data, ['year','month','day','hour','season', 'holiday', 'workingday', 'weather']).describe()
