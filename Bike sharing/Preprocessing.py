0f838379a3d57e94c1e83e99a3a3b2a23085c830
agomariz <ant.gomariz@gmail.com>
2015-12-17 23:36:26 +0000

    refactor

--- Bike sharing/Preprocessing.py
+++ Bike sharing/Preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def _create_dummy(df,column):
     '''
@@ -20,6 +21,7 @@ def process_date_column(df,column):
     df['year'] = pd.to_datetime(df[column]).dt.year
     df['month'] = pd.to_datetime(df[column]).dt.month
     df['day'] = pd.to_datetime(df[column]).dt.day
+    df['weekday'] = pd.to_datetime(df[column]).dt.weekday
     df['hour'] = pd.to_datetime(df[column]).dt.hour
 
     return df.drop(column, 1)
@@ -35,3 +37,14 @@ def get_parts_of_days(df, column):
     df['part_of_day'] = df[column].map(lambda h:_get_part_of_day(h))
     return df
 
+def preprocess(df):
+
+    df = process_date_column(df,'datetime')
+    df = get_parts_of_days(df, 'hour')
+
+    # Uncomment if we want to remove any column
+    # df = df.drop(['month', 'day'], axis=1)
+
+    df = create_dummies(df, ['year','month','day','weekday','hour','season', 'holiday',
+                             'workingday', 'weather', 'part_of_day'])
+    return df
--- Bike sharing/main.py
+++ Bike sharing/main.py
@@ -2,9 +2,5 @@ import pandas as pd
 import Preprocessing as prep
 
 train_data = pd.read_csv('data/train.csv')
-train_data = prep.process_date_column(train_data,'datetime')
-train_data = prep.get_parts_of_days(train_data, 'hour')
-
-print train_data.describe()
-
-print prep.create_dummies(train_data, ['year','month','day','hour','season', 'holiday', 'workingday', 'weather', 'part_of_day']).describe()
+preprocessed_data = prep.preprocess(train_data)
+print preprocessed_data.describe()
